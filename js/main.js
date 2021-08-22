09ec5571b3fb887888105ba88afeb246d711f4ea
SoichiTakaya <57583843+SoichiTakaya@users.noreply.github.com>
2021-08-22 20:51:28 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click', () => {
+    const n = Math.random();
+    if (n < 0.15) {
+      btn.textContent = '大吉'; // 10%
+    } else if (n < 0.4) {
+      btn.textContent = '中吉'; // 30%
+    } else if (n < 0.65) {
+      btn.textContent = '小吉'; // 60%
+    } else if (n < 0.9) {
+      btn.textContent = '末吉'; // 90%
+    } else if (n < 0.97) {
+      btn.textContent = '凶'; // 98%
+    } else {
+      btn.textContent = '大凶'; // 100%
+    }
+  });
+}
